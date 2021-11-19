47f6aa2678183ef900a6b6a2cd7235e45c1e214e
Your Name <you@example.com>
2021-11-20 02:02:01 +0530

    test code

--- sign_up_form.py
+++ sign_up_form.py
@@ -1,49 +1,63 @@
 from driver_call import *
 from home_page import *
-sign_up_locator = By.LINK_TEXT, "Sign up now"
 
 
 def test_sign_up(driver):
     open_home_page(driver)
     open_student_page(driver)
-
-    sign_up = driver.find_element(*sign_up_locator)
-    sign_up.click()
-    sleep(2)
+    open_sign_up_form(driver)
 
     signup_page_title = driver.title
     assert signup_page_title == 'Sign Up'
 
-    sign_up_form(driver)
+    name(driver, "Rina")
+    username(driver, "Rina@example.com")
+    password(driver, "admin@123")
+    confirm_password(driver, "admin@123")
+    submit(driver)
 
     login_page_title = driver.title
     assert login_page_title == 'Login'  # we successfully signed up
 
-def sign_up_form(driver):
 
+def open_sign_up_form(driver):
+    sign_up_locator = By.LINK_TEXT, "Sign up now"
+    sign_up = driver.find_element(*sign_up_locator)
+    sign_up.click()
+    sleep(2)
+
+
+def name(driver, stu_name):
     name_locator = By.NAME, "name"
     name = driver.find_element(*name_locator)
+    name.send_keys(stu_name)
+    sleep(2)
 
+
+def username(driver, u_name):
     username_locator = By.NAME, "username"
     username = driver.find_element(*username_locator)
+    username.send_keys(u_name)
+    sleep(2)
 
+
+def password(driver, passwrd):
     password_locator = By.NAME, "password"
     password = driver.find_element(*password_locator)
+    password.send_keys(passwrd)
+    sleep(2)
 
+
+def confirm_password(driver, confirm_passwrd):
     confirm_password_locator = By.NAME, "confirm_password"
-    confirm_password = driver.find_element(*confirm_password_locator)
+    confirm_ = driver.find_element(*confirm_password_locator)
+    confirm_.send_keys(confirm_passwrd)
+    sleep(2)
 
+
+def submit(driver):
     submit_button_locator = By.XPATH, '//*[@id="form"]/div[5]/input'
     submit_button = driver.find_element(*submit_button_locator)
-
-    name.send_keys("Rina")
-    sleep(2)
-    username.send_keys("rina@example.com")
-    sleep(2)
-    password.send_keys("admin@123")
-    sleep(2)
-    confirm_password.send_keys("admin@123")
-    sleep(2)
     submit_button.click()
     sleep(3)
 
